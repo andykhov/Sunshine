d763a8ae70e00a4fdf3d59326cb6a8d276b8f0ff
Andy <khovandyak@gmail.com>
2017-10-14 19:45:44 -0700

    initial project files

---
+++ Sunshine/app/src/main/java/andykhov/sunshine/ForecastActivity.java
@@ -0,0 +1,13 @@
+package andykhov.sunshine;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ForecastActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+    }
+}
