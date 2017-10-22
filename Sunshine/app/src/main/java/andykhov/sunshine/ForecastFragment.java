98a0712371738d0c46d69dfad93271822877bc5b
Andy <khovandyak@gmail.com>
2017-10-22 00:56:30 -0700

    setup GoogleApiClient

--- Sunshine/app/src/main/java/andykhov/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/andykhov/sunshine/ForecastFragment.java
@@ -2,6 +2,7 @@ package andykhov.sunshine;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -9,14 +10,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
 /**
  * Created by Andy on 10/14/17.
  */
 
 public class ForecastFragment extends Fragment {
+
     RecyclerView mForecastRecyclerView;
     RecyclerView.LayoutManager mForecastLayoutManager;
     ForecastAdapter mForecastAdapter;
+    GoogleApiClient mGoogleApiClient;
 
     public ForecastFragment() {}
 
@@ -28,16 +34,29 @@ public class ForecastFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
 
+        View rootView = inflater.inflate(R.layout.fragment_forecast, container, false);
+        setupRecyclerView(rootView);
+        setupGoogleApiClient();
+
+        return rootView;
+    }
+
+    private void setupRecyclerView(View rootView) {
         mForecastRecyclerView = (RecyclerView) rootView.findViewById(R.id.forecast_list);
         mForecastLayoutManager = new LinearLayoutManager(getContext());
         mForecastAdapter = new ForecastAdapter(createFillerData(20));
-
         mForecastRecyclerView.setLayoutManager(mForecastLayoutManager);
         mForecastRecyclerView.setAdapter(mForecastAdapter);
+    }
 
-        return rootView;
+    private void setupGoogleApiClient() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(getContext())
+                    .enableAutoManage(getActivity(), null)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
     private String[] createFillerData(int size) {
