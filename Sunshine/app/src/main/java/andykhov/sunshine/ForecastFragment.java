66be1c7da8db5ab205efdad8cf792db47cf7b828
Andy <khovandyak@gmail.com>
2017-10-22 22:47:47 -0700

    added location service permission

--- Sunshine/app/src/main/java/andykhov/sunshine/ForecastFragment.java
+++ Sunshine/app/src/main/java/andykhov/sunshine/ForecastFragment.java
@@ -1,8 +1,14 @@
 package andykhov.sunshine;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
@@ -13,16 +19,21 @@ import android.view.ViewGroup;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
+import static android.support.v4.content.ContextCompat.checkSelfPermission;
+
 /**
  * Created by Andy on 10/14/17.
  */
 
-public class ForecastFragment extends Fragment {
+public class ForecastFragment extends Fragment implements GoogleApiClient.ConnectionCallbacks {
+
+    private final int CHECK_PERMISSION_COARSE_LOCATION = 1;
 
     RecyclerView mForecastRecyclerView;
     RecyclerView.LayoutManager mForecastLayoutManager;
     ForecastAdapter mForecastAdapter;
     GoogleApiClient mGoogleApiClient;
+    Location mCurrentLocation;
 
     public ForecastFragment() {}
 
@@ -40,6 +51,39 @@ public class ForecastFragment extends Fragment {
         setupGoogleApiClient();
 
         return rootView;
+    }
+
+    @Override
+    public void onConnected(Bundle connectionHint) {
+        if (!hasLocationPermission())
+            requestLocationPermission();
+    }
+
+    @Override
+    public void onConnectionSuspended(int cause) {}
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] results) {
+        if (requestCode == CHECK_PERMISSION_COARSE_LOCATION) {
+            if (results.length > 0 && results[0] == PackageManager.PERMISSION_GRANTED) {
+                if (Build.VERSION.SDK_INT >= 23 &&
+                        ContextCompat.checkSelfPermission(getContext(),
+                                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+                mCurrentLocation = LocationServices.FusedLocationApi.
+                        getLastLocation(mGoogleApiClient);
+            }
+        }
+    }
+
+    private boolean hasLocationPermission() {
+        return ContextCompat.checkSelfPermission(getContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermission() {
+        String[] permission = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        ActivityCompat.requestPermissions(getActivity(), permission, CHECK_PERMISSION_COARSE_LOCATION);
     }
 
     private void setupRecyclerView(View rootView) {
